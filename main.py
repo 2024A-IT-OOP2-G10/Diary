aaca1fb7bd8e1e044f76fd6111c7bd062654a604
n1yuibb56 <k23111kk@aitech.ac.jp>
2024-11-28 15:54:16 +0900

    ok

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.k23111_diary import k23111_diary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [
+    DiarySample(), 
+    k23111_diary(),
+] 
 
 for d in diaries:
     print("---------------------------------")
