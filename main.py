de419c6c57b36f920a6af61752aee59cc1ff6935
Yosidragon <k23139kk@aitech.ac.jp>
2024-11-28 15:30:43 +0900

    ついかぁ！

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [
+    DiarySample(), 
+    YoshidaDiary(),
+] 
 
 for d in diaries:
     print("---------------------------------")
