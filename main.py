545228aa582977eaa7cc3ceab67f3af122e7b4b0
atsushi0415 <uedaathushi@icloud.com>
2024-11-28 15:36:43 +0900

    篤日記

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.AtsushiDiary import AtshushiDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [DiarySample(),
+           AtshushiDiary(),
+] 
 
 for d in diaries:
     print("---------------------------------")
